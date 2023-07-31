60647d4b7d6cc0d12715a6ddfa7638363751ef75
Animeology <joseph.nguyen19@yahoo.com>
2023-07-30 22:42:52 -0500

    Create Fibonacci.cs

---
+++ FunKatas/Katas/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿namespace FunKatas.Katas
+{
+    public class Fibonacci
+    {
+        public void PrintFibonacci()
+        {
+            Console.WriteLine(FibonacciKata(10));
+        }
+
+        int FibonacciKata(int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            if (num == 1 || num == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return FibonacciKata(num - 1) + FibonacciKata(num - 2);
+            }
+        }
+    }
+}
