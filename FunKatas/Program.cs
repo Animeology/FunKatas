886320127cb033a64cba0af8054d94c1cadef3e5
Animeology <joseph.nguyen19@yahoo.com>
2023-07-30 22:21:30 -0500

    Update Program.cs

--- FunKatas/Program.cs
+++ FunKatas/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace FunKatas
+{
+    using Katas;
+
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.FizzBuzzGame();
+        }
+    }
+}
