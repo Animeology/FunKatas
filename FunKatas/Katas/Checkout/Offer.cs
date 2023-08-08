969249fb0c80ebacb095a84c163c74f6179bba48
Animeology <joseph.nguyen19@yahoo.com>
2023-08-08 14:44:15 -0500

    Create Offer.cs

---
+++ FunKatas/Katas/Checkout/Offer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FunKatas.Katas.Checkout
+{
+    public class Offer
+    {
+        public int Discount { get; set; }
+        public int NumberOfItems { get; set; }
+    }
+}
