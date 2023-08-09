2c11fd3053e417754fbcae73e643241ce2e1b5da
Animeology <joseph.nguyen19@yahoo.com>
2023-08-09 14:19:31 -0500

    Add Method

--- FunKatas/Katas/String Calculator/StringCalculator.cs
+++ FunKatas/Katas/String Calculator/StringCalculator.cs
@@ -1,7 +1,39 @@
-﻿namespace FunKatas.Katas.String_Calculator
+﻿using System.Text.RegularExpressions;
+
+namespace FunKatas.Katas.String_Calculator
 {
+    using FunKatas.KataTest.StringCalculatorTests;
+
     public class StringCalculator
     {
+        public void StartCalculator()
+        {
+            StringCalculatorTests tests = new StringCalculatorTests();
+            tests.EmptyString_ReturnsZero();
 
+            Console.Write("String input: ");
+            string input = Console.ReadLine()!;
+
+            Add(input);
+        }
+
+
+        public int Add(string nums)
+        {
+            int sum = 0;
+
+            MatchCollection matches = Regex.Matches(nums, @"\d+");
+
+            foreach (Match match in matches)
+            {
+                int number;
+                if (int.TryParse(match.Value, out number))
+                {
+                    sum += number;
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+            return sum;
+        }
     }
 }
