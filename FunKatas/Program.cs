7c3d00326cf6a0f5e7efe5718596c4cf1bb9a505
Animeology <joseph.nguyen19@yahoo.com>
2023-08-09 14:19:45 -0500

    Added String Calculator Instance

--- FunKatas/Program.cs
+++ FunKatas/Program.cs
@@ -3,6 +3,7 @@
     using Katas.Checkout;
     using Katas.Fibonacci;
     using Katas.FizzBuzz;
+    using Katas.String_Calculator;
 
     public class Program
     {
@@ -16,6 +17,9 @@
 
             //CheckoutKata checkout = new CheckoutKata();
             //checkout.Checkout();
+
+            StringCalculator calculator = new StringCalculator();
+            calculator.StartCalculator();
         }
     }
 }
