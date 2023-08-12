92ddfa73c2921af48fd3404986d985c6e2f2b091
Animeology <joseph.nguyen19@yahoo.com>
2023-08-11 19:36:47 -0500

    Moved Methods

--- FunKatas/Katas/BowlingGame/BowlingGame.cs
+++ FunKatas/Katas/BowlingGame/BowlingGame.cs
@@ -10,24 +10,16 @@
         public void Bowling()
         {
             BowlingAttempts();
+            DisplayScore();
         }
 
-        bool IsSpare()
-        {
-            return Attempts == 2 && KnockedDownPins == TOTAL_PINS ? true : false;
-        }
-
-        bool IsStrike()
-        {
-            return Attempts == 1 && KnockedDownPins == TOTAL_PINS ? true : false;
-        }
 
         void BowlingAttempts()
         {
             for (int i = 1; i <= TOTAL_FRAMES; i++)
             {
                 Random random = new Random();
-                KnockedDownPins = random.Next(EMPTY_FRAME, Math.Min(LeftoverPins, TOTAL_PINS));
+                KnockedDownPins = random.Next(EMPTY_FRAME, Math.Min(LeftoverPins, TOTAL_PINS) + 1);
 
                 LeftoverPins = LeftoverPins - KnockedDownPins;
                 Attempts++;
@@ -42,14 +34,46 @@
                 if (IsStrike())
                 {
                     Console.WriteLine("It's a STRIKE!!!");
+                    if(Attempts == 2)
+                    {
+                        Score += KnockedDownPins;
+                    }
                 }
                 else if (IsSpare())
                 {
                     Console.WriteLine("Spare");
+                    if (Attempts == 3)
+                    {
+                        Score += KnockedDownPins;
+                    }
                 }
+
+                Score += KnockedDownPins;
 
                 Console.WriteLine($"Frame {i}: Knocked Pins: {KnockedDownPins} Pins Left: {LeftoverPins}");
             }
+        }
+        bool IsSpare()
+        {
+            if(Attempts == 2 && KnockedDownPins == TOTAL_PINS)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool IsStrike()
+        {
+            if (Attempts == 1 && KnockedDownPins == TOTAL_PINS)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        void DisplayScore()
+        {
+            Console.WriteLine($"Score: {Score}");
         }
 
         const int EMPTY_FRAME = 0;
