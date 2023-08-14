b802ec364dca6a4febb28062e2d27c88d4136aca
Animeology <joseph.nguyen19@yahoo.com>
2023-08-14 15:40:42 -0500

    Tested Five Pins and Ten Pins

--- FunKatas/KataTest/BowlingGameTests/BowlingGameTests.cs
+++ FunKatas/KataTest/BowlingGameTests/BowlingGameTests.cs
@@ -8,13 +8,41 @@
         {
             BowlingGame game = new BowlingGame();
 
-            game.KnockedDownPins = 0;
+            game.MockKnockedPins = 0;
 
             game.BowlingAttempt();
 
             if(game.Score != 0)
             {
                 throw new Exception("Zero Pins doesn't return 0");
+            }
+        }
+
+        public void HitFivePins_ReturnFive()
+        {
+            BowlingGame game = new BowlingGame();
+
+            game.MockKnockedPins = 5;
+
+            game.BowlingAttempt();
+
+            if (game.Score != 5)
+            {
+                throw new Exception("Five Pins doesn't return 5");
+            }
+        }
+
+        public void HitTenPins_ReturnTen()
+        {
+            BowlingGame game = new BowlingGame();
+
+            game.MockKnockedPins = 10;
+
+            game.BowlingAttempt();
+
+            if (game.Score != 10)
+            {
+                throw new Exception("Ten Pins doesn't return 10");
             }
         }
     }
